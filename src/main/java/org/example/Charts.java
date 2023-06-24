465f096f0b448fb511db4fdaa2131261bf361931
lior <liorb014@gmail.com>
2023-06-25 01:21:57 +0300

    hi

--- src/main/java/org/example/Charts.java
+++ src/main/java/org/example/Charts.java
@@ -14,13 +14,15 @@ public class Charts extends Panel {
     private String timeData;
     private String requestData;
     private int totalAmountOfRequest;
+    private final int WIDTH_MARGIN = 10;
+    private final int HEIGHT_MARGIN = 40;
 
     public Charts(MessagesBot bot) {
         super(bot);
-        this.totalAmountOfRequest=0;
+        this.totalAmountOfRequest = 0;
         QuickChart chart = new QuickChart();
-        chart.setWidth(490);
-        chart.setHeight(460);
+        chart.setWidth(WINDOW_SIZE - WIDTH_MARGIN);
+        chart.setHeight(WINDOW_SIZE - HEIGHT_MARGIN);
         this.timeData = "labels: []";
         this.requestData = "data: []";
         chart.setBackgroundColor("white");
@@ -46,30 +48,32 @@ public class Charts extends Panel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-            g.drawImage(icon.getImage(), 0, 0, this);
+        g.drawImage(icon.getImage(), 0, 0, this);
     }
 
     private void throughTime() {
         SimpleDateFormat formatter = new SimpleDateFormat("hh:mm");
         Date currentDate = new Date();
-        this.timeData=this.timeData.replace("]", ",'"+formatter.format(currentDate)+"']");
-        this.timeData=  this.timeData.replace(timeData.charAt(9),' ');
+        this.timeData = this.timeData.replace("]", ",'" + formatter.format(currentDate) + "']");
+        this.timeData = this.timeData.replace(timeData.charAt(9), ' ');
     }
-    private void requestAmount(){
+
+    private void requestAmount() {
         int temp = getAmountOfRequests();
-        this.requestData = this.requestData.replace("]",",'"+(getAmountOfRequests()-totalAmountOfRequest)+"']");
-        this.requestData =  this.requestData.replace(this.requestData.charAt(7),' ');
-        totalAmountOfRequest=temp;
+        this.requestData = this.requestData.replace("]", ",'" + (getAmountOfRequests() - totalAmountOfRequest) + "']");
+        this.requestData = this.requestData.replace(this.requestData.charAt(7), ' ');
+        totalAmountOfRequest = temp;
     }
+
     public int getAmountOfRequests() {
         return super.getBot().getUpdateList().size();
     }
 
-    private void setConfig(QuickChart chart){
+    private void setConfig(QuickChart chart) {
         chart.setConfig("      {\n" +
                 "                  type: 'line',\n" +
                 "                  data: {\n" +
-                this.timeData +",\n" +
+                this.timeData + ",\n" +
                 "                                \n" +
                 "                    datasets: [\n" +
                 "                      {\n" +
@@ -78,7 +82,7 @@ public class Charts extends Panel {
                 "                        borderColor: 'rgb(54, 162, 235)',\n" +
                 "                        borderWidth: 2,\n" +
                 "                        fill: true,\n" +
-                "                       "+this.requestData+ ",\n" +
+                "                       " + this.requestData + ",\n" +
                 "                      },\n" +
                 "                                 ],\n" +
                 "                  },\n" +
